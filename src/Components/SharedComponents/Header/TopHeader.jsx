71d007a95ab9705cea9343681d4d873aa1633db9
emon ahmed <emon9996a@gmail.com>
2026-04-30 09:01:47 +0600

    top header update

--- src/Components/SharedComponents/Header/TopHeader.jsx
+++ src/Components/SharedComponents/Header/TopHeader.jsx
@@ -1,16 +1,34 @@
 import React from "react";
+import { FaSquarePhone } from "react-icons/fa6";
 import { IoLogoWhatsapp } from "react-icons/io";
 
 const TopHeader = () => {
   return (
-    <>
-      <div>
-        <div className="flex items-center gap-2">
-          <IoLogoWhatsapp />
-          <p className="text-[12px] ct">01310258092</p>
+    <div className="bg-[#f8f8fb] py-2 ">
+      <div className="flex justify-between items-center container mx-auto px-24 ">
+        <div className="flex gap-12 items-center  ct  ">
+          <div className="flex items-center gap-2">
+            <IoLogoWhatsapp />
+            <p className="text-[12px] ct">01310258092</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <FaSquarePhone />
+            <p className="text-[12px] ct">01310258092</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-[12px] ct">
+            world's Fastest online Shoping Destination
+          </p>
+        </div>
+        <div className="flex items-center text-[12px] gap-5">
+          <p>Help?</p>
+          <p>Trac order?</p>
+          <p>English</p>
+          <p>Contact us</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
