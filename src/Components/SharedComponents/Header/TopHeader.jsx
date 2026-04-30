4bb77f3ca28965752814686779e1166d174b7d66
emon ahmed <emon9996a@gmail.com>
2026-04-30 08:45:29 +0600

    update

--- src/Components/SharedComponents/Header/TopHeader.jsx
+++ src/Components/SharedComponents/Header/TopHeader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { IoLogoWhatsapp } from "react-icons/io";
 
 const TopHeader = () => {
-  return <div>TopHeader</div>;
+  return (
+    <>
+      <div>
+        <div className="flex items-center gap-2">
+          <IoLogoWhatsapp />
+          <p className="text-[12px] ct">01310258092</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TopHeader;
