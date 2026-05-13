288b35e1742cef568de62fe26d32a1041a7b9886
emon ahmed <emon9996a@gmail.com>
2026-05-13 20:59:50 +0600

    make Hook

--- src/Hooks/useData.js
+++ src/Hooks/useData.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 const useData = () => {
-
-
-
+     const [cetagorys, setCategory] = useState([]);
+  useEffect(() => {
+    fetch("category.json")
+      .then((res) => res.json())
+      .then((data) => setCategory(data));
+  }, []);
     
-  return (
-    <div>useData</div>
-  )
+         const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("Products.json")
+      .then((res) => res.json())
+      .then((data) =>setProducts(data));
+  }, []);
+
+
+  return {cetagorys,products}
 }
 
 export default useData
--- src/Page/Home/HomeComponents/HomeCetagory.jsx
+++ src/Page/Home/HomeComponents/HomeCetagory.jsx
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from "react";
+import useData from "../../../Hooks/useData";
 
 const HomeCetagory = () => {
-  const [cetagory, setCategory] = useState([]);
-  useEffect(() => {
-    fetch("category.json")
-      .then((res) => res.json())
-      .then((data) => setCategory(data));
-  }, []);
+  const { cetagorys } = useData();
+
   return (
     <div className="container mx-auto px-24">
       <div className="flex justify-center gap-8">
-        {cetagory.map((category) => (
+        {cetagorys.map(category=> (
           <div className=" flex flex-col items-center bg-gray-100 px-16 rounded-md py-4">
-                <img className="w-16 h-16 mb-3" src={category.image} alt="" />
-                <p>{category.name}</p>
+            <img className="w-16 h-16 mb-3" src={category.image} alt="" />
+            <p>{category.name}</p>
             <p className="text-xs text-gray-400">{category.items} items</p>
           </div>
         ))}
