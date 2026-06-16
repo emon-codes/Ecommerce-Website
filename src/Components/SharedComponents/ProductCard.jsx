3bae3c1449dc72db451be44d31877588174993d2
emon ahmed <emon9996a@gmail.com>
2026-06-16 23:02:37 +0600

    productCart update

--- src/Components/SharedComponents/ProductCard.jsx
+++ src/Components/SharedComponents/ProductCard.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import { FaBangladeshiTakaSign } from "react-icons/fa6";
+import { IoStarHalfOutline } from "react-icons/io5";
 
 const ProductCard = ({ product }) => {
   console.log(product);
 
   return (
     <div>
-      <h1>{product?.name}</h1>
+      <div className="w-74 flex flex-col justify-center items-center border border-gray-200 rounded-md" >
+        <img className="w-72" src={product.image} alt="" />
+      </div>
+      <div>
+        <p>{product.categoryName}</p>
+        <h3>{product.name}</h3>
+        <div className="flex gap-1 text-orange-500">
+          <span><IoStarHalfOutline /></span>
+          <span><IoStarHalfOutline /></span>
+          <span><IoStarHalfOutline /></span>
+          <span><IoStarHalfOutline /></span>
+          <span><IoStarHalfOutline /></span>
+        </div>
+        <div className="flex gap-3 ">
+          <p >{product.price}</p> <FaBangladeshiTakaSign />
+          <p className="line-through text-gray-500">{product.mrp}</p> <FaBangladeshiTakaSign />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
